749905a3b3179f36b8b8d1adc80e32256af659d7
12leesh <12leesh@daum.net>
2020-05-21 23:47:06 +0900

    added

--- bibi.py
+++ bibi.py
@@ -5,3 +5,10 @@ for o in range(1,4) :
 
 
 a = 3
+
+'''
+Successfully created project 'hoho1' on GitHub, but initial commit failed: *** Please tell me who you are. Run git config --global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository. unable to auto-detect email address (got 'Administrator@PC046.(none)')
+
+'''
+
+# 34
